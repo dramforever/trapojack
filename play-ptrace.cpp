26303b8452abb78474597f772f14bfa717ca34ec
dramforever <catmimiao@me.com>
2016-01-16 19:39:45 +0800

    Add memory, time and output limits

--- play-ptrace.cpp
+++ play-ptrace.cpp
@@ -3,16 +3,19 @@ extern "C"
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 #include <unistd.h>
 #include <sys/user.h>
 #include <sys/reg.h>
 #include <sys/syscall.h>
 #include <asm/unistd.h>
 #include <errno.h>
+#include <sys/resource.h>
 }
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -37,11 +40,7 @@ inline bool is_safe_call(const user_regs_struct *regs)
     || (id == __NR_mmap && (regs->r8 == 0xffffffffULL))
 
     // stat-ing stdin or stdout
-    || (id == __NR_fstat && (regs->rdi == 0 || regs->rdi == 1))
-
-    // Exiting
-    || id == __NR_exit
-    || id == __NR_exit_group;
+    || (id == __NR_fstat && (regs->rdi == 0 || regs->rdi == 1));
 }
 
 inline bool should_mask_call(const user_regs_struct *regs)
@@ -49,21 +48,40 @@ inline bool should_mask_call(const user_regs_struct *regs)
   return regs->orig_rax == __NR_uname || regs->orig_rax == __NR_readlink;
 }
 
-inline void filter_syscall(pid_t child)
+inline bool is_exit_call(const user_regs_struct *regs)
+{
+  return regs->orig_rax == __NR_exit || regs->orig_rax == __NR_exit_group;
+}
+
+#define TRY(x)								\
+  do {									\
+    if((x) == -1) {							\
+      fprintf(stderr, "[exe]: Internal error %s\n", strerror(errno));	\
+      kill(child, SIGKILL);						\
+      asm("int $3"); exit(-1);						\
+    }									\
+  } while(0)
+
+inline void filter_syscall(pid_t child, long long *out_lim)
 {
   user_regs_struct regs;
-  ptrace(PTRACE_GETREGS, child, NULL, &regs);
+  TRY(ptrace(PTRACE_GETREGS, child, NULL, &regs));
 
-  fflush(stderr);
-  if(should_mask_call(&regs)) {
+  TRY(fflush(stderr));
+
+  if(regs.orig_rax == __NR_write && regs.rdi == 1
+     && (*out_lim -= (long long)regs.rdx) <= 0) {
+    // Write to stdout, OLE
+    fputs("[exe]: Output limit exceeded\n", stderr);
+    kill(child, SIGKILL);
+    exit(1);
+  } else if(is_exit_call(&regs)) {
+    TRY(ptrace(PTRACE_SYSCALL, child, 0, 0));
+  } else if(is_safe_call(&regs) || should_mask_call(&regs)) {
     // TODO: Implement masks
-    ptrace(PTRACE_SYSCALL, child, 0, 0);
-    wait(NULL);
-    ptrace(PTRACE_SYSCALL, child, 0, 0);
-  } else if(is_safe_call(&regs)) {
-    ptrace(PTRACE_SYSCALL, child, 0, 0);
-    wait(NULL);
-    ptrace(PTRACE_SYSCALL, child, 0, 0);
+    TRY(ptrace(PTRACE_SYSCALL, child, 0, 0));
+    TRY(wait(NULL));
+    TRY(ptrace(PTRACE_SYSCALL, child, 0, 0));
   } else {
     fprintf(stderr, "[exe]: Unsafe call %llu, killing guest\n",
 	    regs.orig_rax);
@@ -74,50 +92,109 @@ inline void filter_syscall(pid_t child)
 
 int main(int argc, char *argv[], char *envp[])
 {
-  if(argc <= 1)
-    {
-      fputs("Usage: play-ptrace <program> [<args>...]\n", stderr);
-      return 1;
-    }
+  if(argc <= 6) {
+    fputs("Usage: play-ptrace <input> <output> "
+	  "<time limit> <mem limit> <out limit>"
+	  "<program> [<args>...]\n", stderr);
+    return -1;
+  }
+
+  long long time_lim = strtoll(argv[3], &argv[3], 10);
+
+  if(*argv[3] != '\0') {
+    fputs("Time limit must be a number\n", stderr);
+    return -1;
+  }
+
+  long mem_lim = strtol(argv[4], &argv[4], 10);
+
+  if(*argv[4] != '\0') {
+    fputs("Mem limit must be a number\n", stderr);
+    return -1;
+  }
+
+  long long out_lim = strtoll(argv[5], &argv[5], 10);
+
+  if(*argv[5] != '\0') {
+    fputs("Output limit must be a number\n", stderr);
+    return -1;
+  }
   
   pid_t child = vfork();
   
   if(child == 0) {
-    ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-    execve(argv[1], argv+1, envp);
-    _exit(127);
+
+#define GTRY(x, ec)							\
+    do {								\
+      if((x) == ec) {							\
+	fprintf(stderr, "[gst]: Internal error %s\n", strerror(errno)); \
+	fflush(stderr);							\
+	_exit(-1);							\
+      }									\
+    } while(0)
+
+    fprintf(stderr, "[gst]: Guest pid = %d\n", getpid());
+
+    GTRY(freopen(argv[1], "r", stdin), NULL);
+    GTRY(freopen(argv[2], "w", stdout), NULL);
+    GTRY(ptrace(PTRACE_TRACEME, 0, NULL, NULL), -1);
+
+    rlimit mlm;
+    GTRY(getrlimit(RLIMIT_AS, &mlm), -1);
+    mlm.rlim_cur = mlm.rlim_max = mem_lim;
+    GTRY(setrlimit(RLIMIT_AS, &mlm), -1);
+
+    itimerval tmr;
+
+    GTRY(getitimer(ITIMER_REAL, &tmr), -1);
+
+    tmr.it_value.tv_sec = time_lim / 1000000;
+    tmr.it_value.tv_usec = time_lim % 1000000;
+
+    GTRY(setitimer(ITIMER_REAL, &tmr, NULL), -1);
+
+    execve(argv[6], argv+6, envp);
+
+    // execve failed
+    _exit(1);
   } else {
     int status;
     wait(&status);
-    bool exec_failed = WIFEXITED(status);
-    ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_TRACESYSGOOD);
-    ptrace(PTRACE_SYSCALL, child, 0, 0); // Skip over the exec
-    wait(&status);
-    while(WIFSTOPPED(status)) {
-      if(WSTOPSIG(status) == (SIGTRAP | 0x80)) {
-	filter_syscall(child);
-      } else {
-	ptrace(PTRACE_CONT, child, NULL, WSTOPSIG(status));
-      }
-      wait(&status);
-    }
-
-    if(exec_failed) {
-      fputs("[exe]: Exec failed in guest\n", stderr);
+    if(WIFEXITED(status)) {
+      fputs("[exe]: Failed to start guest\n", stderr);
       return 127;
-    } else if (WIFEXITED(status)) {
-      if(WEXITSTATUS(status) == 0) {
-	fputs("[exe]: Exited successfully\n", stderr);
-	return 0;
-      } else {
-	fprintf(stderr, "[exe]: Exited with failure status %d\n",
-		WEXITSTATUS(status));
-	return 1;
+    } else {
+      TRY(ptrace(PTRACE_SETOPTIONS, child, NULL, PTRACE_O_TRACESYSGOOD));
+      TRY(ptrace(PTRACE_SYSCALL, child, 0, 0)); // Skip over the exec
+      TRY(wait(&status));
+      while(WIFSTOPPED(status)) {
+	if(WSTOPSIG(status) == (SIGTRAP | 0x80)) {
+	  filter_syscall(child, &out_lim);
+	} else {
+	  ptrace(PTRACE_CONT, child, NULL, WSTOPSIG(status));
+	}
+	wait(&status);
       }
-    } else if (WIFSIGNALED(status)) {
-      fprintf(stderr, "[exe]: Killed by signal %d\n",
-	      WTERMSIG(status));
-      return 1;
+
+      if (WIFEXITED(status)) {
+	if(WEXITSTATUS(status) == 0) {
+	  fputs("[exe]: Exited successfully\n", stderr);
+	  return 0;
+	} else {
+	  fprintf(stderr, "[exe]: Exited with failure status %d\n",
+		  WEXITSTATUS(status));
+	  return 1;
+	}
+      } else if (WIFSIGNALED(status)) {
+	if(WTERMSIG(status) == SIGALRM) {
+	  fputs("[exe]: Time limit exceeded", stderr);
+	  return 1;
+	} else {
+	  fprintf(stderr, "[exe]: Killed by signal %d\n",
+		  WTERMSIG(status));
+	  return 1;
+	}
+      }
     }
   } 
 }
